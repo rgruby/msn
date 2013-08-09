51afe7c69d21ed291e12572acdb165b663de424b
Ed Raley <ed@huddler-inc.com>
2013-08-09 19:12:55 -0400

    Add Gemfile, secret_token, RSpec, README.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,5 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
 # Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
@@ -9,4 +7,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Msn::Application.config.secret_key_base = '827203279da04a728cb1ea04b53b139c5edc3f2c9cf0466c059092bb6fbbcc47833385b38a48a22c53b2afa31436f01077397f0ccde49bc42533670920a8e457'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Msn::Application.config.secret_key_base = secure_token
+
