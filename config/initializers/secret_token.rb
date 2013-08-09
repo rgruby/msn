c6d48b7d34bff27c7232166e97651107513f9123
Ed Raley <ed@huddler-inc.com>
2013-08-09 19:00:38 -0400

    Initial commit after 'rails new'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Msn::Application.config.secret_key_base = '827203279da04a728cb1ea04b53b139c5edc3f2c9cf0466c059092bb6fbbcc47833385b38a48a22c53b2afa31436f01077397f0ccde49bc42533670920a8e457'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Msn::Application.config.session_store :cookie_store, key: '_msn_session'
