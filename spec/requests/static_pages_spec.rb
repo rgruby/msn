78952623b58aebf5701c9e9abdee2c1e13b38174
Ed Raley <ed@huddler-inc.com>
2013-08-10 11:13:22 -0400

    Add Twitter Bootstrap.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,49 +2,49 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title) { "Missionary Support Network" }
+  let(:base_title) { "Missionary Support Network" }
 
-	describe "Home page" do
+  describe "Home page" do
 
-		it "should have the content 'MSN Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('MSN Home')
-		end
+    it "should have the content 'Welcome to the Missionary Support Network'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Welcome to the Missionary Support Network')
+    end
 
-		it "should have the base title" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Missionary Support Network")
-		end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Missionary Support Network")
+    end
 
-		it "should not have a custom page title" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title('| Home')
-		end
-	end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+  end
 
-	describe "Help page" do
+  describe "Help page" do
 
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
 
-		it "should have the right title" do
-			visit '/static_pages/help'
-			expect(page).to have_title("#{base_title} | Help")
-		end
-	end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
 
-	describe "About page" do
+  describe "About page" do
 
-		it "should have the content 'AboutSN'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About MSN')
-		end
+    it "should have the content 'About MSN'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About MSN')
+    end
 
-		it "should have the right title" do
-			visit '/static_pages/about'
-			expect(page).to have_title("#{base_title} | About")
-		end
-	end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
 end
