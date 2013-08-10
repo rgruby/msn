56f96301e8d65b349c38129f72f7ddaf26d7ec1e
Ed Raley <ed@huddler-inc.com>
2013-08-09 23:51:46 -0400

    Add testing support: spork, guard and some static_pages tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Missionary Support Network" }
+
 	describe "Home page" do
 
 		it "should have the content 'MSN Home'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Missionary Support Network | Home")
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Missionary Support Network | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -37,7 +39,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Missionary Support Network | About")
+			expect(page).to have_title("#{base_title} | About")
 		end
 	end
 end
